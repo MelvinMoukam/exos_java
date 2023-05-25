df9f39cc91755446e37db9aa082f207de6d25514
moukammelvin.com <moukammelvin@gmail.com>
2023-05-25 17:00:22 +0200

    "message de bienvenue"

---
+++ src/Exercice1/Bienvenue.java
@@ -0,0 +1,18 @@
+package Exercice1;
+
+import java.util.Scanner;
+
+public class Bienvenue {
+
+    public void methodeBienvenue (){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Entrez votre nom : ");
+
+        String nom = sc.nextLine();
+
+        System.out.println("Bienvenue " +nom + "!");
+        
+    }
+}

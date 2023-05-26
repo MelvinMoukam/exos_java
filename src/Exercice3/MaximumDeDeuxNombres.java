aeba3ad3f3e7281221bfb1587167bce4a832ba4b
moukammelvin.com <moukammelvin@gmail.com>
2023-05-27 00:02:27 +0200

    changement du type de variable du nombre récupéré

---
+++ src/Exercice3/MaximumDeDeuxNombres.java
@@ -0,0 +1,28 @@
+package Exercice3;
+
+import java.util.Scanner;
+
+public class MaximumDeDeuxNombres {
+
+    public void maximumNumber(){
+        System.out.println("Entrez votre premier nombre réel :");
+        Scanner number1 = new Scanner(System.in);
+        float n1 = number1.nextFloat();
+
+        System.out.println("Entrez votre second nombre réel :");
+        Scanner number2 = new Scanner(System.in);
+        float n2 = number2.nextFloat();
+
+
+        if( n1 < n2){
+
+            System.out.println("le plus grand entre les deux nombres entrés est : " +n2);
+
+        }else{
+
+            System.out.println("le plus grand entre les deux nombres entrés est : " +n1);
+        }
+
+
+    }
+}

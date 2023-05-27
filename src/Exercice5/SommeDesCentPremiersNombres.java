3c116e7b09a4c056502d108aa2145554163a5cb2
moukammelvin.com <moukammelvin@gmail.com>
2023-05-27 02:09:43 +0200

    classe principale

---
+++ src/Exercice5/SommeDesCentPremiersNombres.java
@@ -0,0 +1,15 @@
+package Exercice5;
+
+public class SommeDesCentPremiersNombres {
+
+    public void sommeDesCentspremiersNombres(){
+
+        int s =0;
+
+        for(int i= 0; i<=100; i++){
+
+            s=s+i;
+        }
+        System.out.println("la somme des 100 premiers nombres est : " +s);
+    }
+}

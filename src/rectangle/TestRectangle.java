9b01f296701288c8caa9b5065106aef3f84c6159
moukammelvin.com <moukammelvin@gmail.com>
2023-05-29 16:38:08 +0200

    test classe rectangle

---
+++ src/rectangle/TestRectangle.java
@@ -0,0 +1,11 @@
+package rectangle;
+
+public class TestRectangle {
+
+    public static void main(String[] args) {
+
+        Rectangle rectangle= new Rectangle (9.0 , 5.0);
+
+        rectangle.afficher();
+    }
+}

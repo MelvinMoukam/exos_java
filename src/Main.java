cd1a6add1d10042ed7ec40cd77c1b03d4e15b148
moukammelvin.com <moukammelvin@gmail.com>
2023-05-29 13:11:59 +0200

    methode principale

--- src/Main.java
+++ src/Main.java
@@ -3,25 +3,35 @@ import Exercice2.DoubleNombre;
 import Exercice3.MaximumDeDeuxNombres;
 import Exercice4.PairImpair;
 import Exercice5.SommeDesCentPremiersNombres;
+import Exercice5.SommeNpremiersNombres;
 
 
 public class Main {
 
     public static void main(String[]  arg){
 
+        // message de bienvenue
         Bienvenue bienvenue = new Bienvenue();
         bienvenue.methodeBienvenue();
 
+         //Double d'un nombre
         DoubleNombre number = new DoubleNombre();
         number.doublenumber();
 
+         //Maximum de deux nombres
         MaximumDeDeuxNombres maximum =new MaximumDeDeuxNombres();
         maximum.maximumNumber();
 
+        //Nombre pair ou impair
         PairImpair r = new PairImpair();
         r.nombrePairImpair();
 
+        // somme des 100 premiers nombres entiers
         SommeDesCentPremiersNombres somme = new SommeDesCentPremiersNombres();
         somme.sommeDesCentspremiersNombres();
+
+        // somme des n premiers nombres entiers
+        SommeNpremiersNombres so = new SommeNpremiersNombres();
+        so.sommesNpremiersNombres();
     }
 }

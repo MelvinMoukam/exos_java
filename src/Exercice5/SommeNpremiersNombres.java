ba80d44fa3ca6ad24529bac135fdaddf937b8cd4
moukammelvin.com <moukammelvin@gmail.com>
2023-05-29 13:13:30 +0200

    SommeNPremiersNombres

---
+++ src/Exercice5/SommeNpremiersNombres.java
@@ -0,0 +1,23 @@
+package Exercice5;
+
+import java.util.Scanner;
+
+public class SommeNpremiersNombres {
+
+    public void sommesNpremiersNombres(){
+
+        System.out.println("Entrez un nombre aléatoire : ");
+
+        Scanner nbre_aleatoire = new Scanner(System.in);
+
+        int result = nbre_aleatoire.nextInt();
+
+        int s = 0;
+
+        for (int i =0 ; i<= result; i++){
+
+            s+=i;
+        }
+        System.out.println("la somme des 100 premiers nombres est : " +s);
+    }
+}
